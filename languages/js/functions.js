2fa23a43b1c74cb77fe6d7dd6d31073c733512f3
Teeheene <diane.panganiban1010@gmail.com>
2025-07-24 05:55:32 +0800

    practice js functions

---
+++ languages/js/functions.js
@@ -0,0 +1,26 @@
+const myText = "Hi, im a string!";
+const string = myText.replace("string", "Dawg");
+
+console.log(string);
+
+const myArr = ["I", "love", "coding"];
+const joinedString = myArr.join(" ");
+
+console.log(joinedString);
+
+const myNum = Math.random();
+
+console.log(myNum);
+
+//lets try functions meow
+function helloFunc()
+{
+	console.log("Hello World!");
+}
+
+helloFunc();
+
+//anon function
+(function() {
+	console.log("hello");
+});
